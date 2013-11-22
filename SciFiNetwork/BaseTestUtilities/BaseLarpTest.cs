edbe2fae69a9c3e45bdb6594b6a423b068b4ba60
Luca Zenari <lozeno1982@gmail.com>
2013-11-22 15:54:00 +0000

    base class for database integration tests

--- SciFiNetwork/BaseTestUtilities/BaseLarpTest.cs
+++ SciFiNetwork/BaseTestUtilities/BaseLarpTest.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataMappings;
 using NUnit.Framework;
 
 namespace BaseTestUtilities
@@ -10,5 +12,13 @@ namespace BaseTestUtilities
     [TestFixture]
     public abstract class BaseLarpTest
     {
+        [TestFixtureSetUp]
+        public void CreateInMemoryDatabase()
+        {
+            using (var unitOfWork = new gamedata2Entities())
+            {
+                unitOfWork.Database.Create();
+            }
+        }
     }
 }
