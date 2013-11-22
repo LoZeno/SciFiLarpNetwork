0d33e210127750d4a1158ec55939bf08bf6c7d0c
Luca Zenari <lozeno1982@gmail.com>
2013-11-22 15:39:33 +0000

    GuidQRCodeManager library

---
+++ SciFiNetwork/QRCodeLib.Test/GuidCodeManagerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace QRCodeLib.Test
+{
+    [TestFixture]
+    public class GuidCodeManagerTest
+    {
+        [TestCase("ba0e5046-e9c4-45ac-bf79-47a9fb1581e9")]
+        public void Test_WhenQrCodeIsPassed_ReturnsGUID(string guid)
+        {
+            Image qrcodeToTest = Image.FromFile(@"Resources\" + guid + ".png");
+            var result = GuidCodeManager.GetCodeFromPicture(qrcodeToTest);
+            Assert.AreEqual(guid, result);
+        }
+
+        [TestCase("ba0e5046-e9c4-45ac-bf79-47a9fb1581e9")]
+        public void Test_WhenGuidIsPassed_ReturnsCorrectPicture(string guid)
+        {
+            var pictureToGet = Image.FromFile(@"Resources\" + guid + ".png");
+            var newPicture = GuidCodeManager.GetPictureFromText(guid);
+            Assert.AreEqual(pictureToGet.Height, newPicture.Height);
+            Assert.AreEqual(pictureToGet.Width, newPicture.Width);
+        }
+    }
+}
---
+++ SciFiNetwork/QRCodeLib/GuidCodeManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace QRCodeLib
+{
+    public static class GuidCodeManager
+    {
+        public static Image GetPictureFromText(string code)
+		{
+			QRCodeEncoder encoder = new QRCodeEncoder();
+			encoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE; //ALPHA_NUMERIC, nonostante il nome, non va bene per codificare un GUID.
+			encoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.L;
+			encoder.QRCodeVersion = 3;
+
+			return encoder.Encode(code);
+		}
+
+		public static Image GetPictureFromGuid(Guid? uniqueCode)
+		{
+			return GetPictureFromText(uniqueCode.ToString());
+		}
+
+		public static string GetCodeFromPicture(Bitmap pic)
+		{
+			QRCodeDecoder decoder = new QRCodeDecoder();
+			QRCodeBitmapImage image = new QRCodeBitmapImage(pic);
+			return decoder.Decode(image);
+		}
+
+		public static string GetCodeFromPicture(Image pic)
+		{
+			Bitmap picture = new Bitmap(pic);
+			return GetCodeFromPicture(picture);
+		}
+    }
+}
